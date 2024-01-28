a9c73f602fca6a24b11069cb0a570fa3d19747c3
Your Name <megafm2@gmail.com>
2024-01-28 16:10:15 +0200

    yy

---
+++ cool/Darbas/Portfolio copy/js/scipt.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('#menu-icon');
+let navbar =document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+    
+}
+navbar.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
